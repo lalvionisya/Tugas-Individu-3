3db230487eab88a29253772b2a025a960081fc14
lidia <104456433+lalvionisya@users.noreply.github.com>
2023-03-18 12:53:50 +0700

    Add files via upload

---
+++ data.jsx
@@ -0,0 +1,189 @@
+
+export const MASKAPAI = {
+    GRD:{
+        maskapai_id: "GRD",
+        maskapai_nama: "Garuda Indonesia",
+        maskapai_logo: "../images/garuda.png", 
+    },
+    SJW:{
+        maskapai_id: "SJW",
+        maskapai_nama: "Sriwijaya Air",
+        maskapai_logo: "../images/Sriwijaya_air.png", 
+    },
+    BTK:{
+        maskapai_id: "BTK",
+        maskapai_nama: "Batik Air",
+        maskapai_logo: "../images/batik.png", 
+    },
+    WNG:{
+        maskapai_id: "WNG",
+        maskapai_nama: "Wings Air",
+        maskapai_logo: "../images/wings.png",
+    },
+    AJT:{
+        maskapai_id: "AJT",
+        maskapai_nama: "Super Air Jet",
+        maskapai_logo: "../images/airjet.png",
+    },
+    CTL:{
+        maskapai_id: "CTL",
+        maskapai_nama: "Citilink",
+        maskapai_logo: "../images/citilink.png",
+    },
+    LIO:{
+        maskapai_id: "LIO",
+        maskapai_nama: "Lion Air",
+        maskapai_logo: "../images/lion.jpg",
+    },
+};
+
+export const BANDARA = {
+    SKH:{
+        bandara_kode: "SKH",
+        bandara_nama: "Soekarno Hatta",
+    },
+    RDN:{
+        bandara_kode: "RDN",
+        bandara_nama: "Radin Inten",
+    },
+    NGR:{
+        bandara_kode: "NGR",
+        bandara_nama: "Ngurah Rai",
+    },
+    JND:{
+        bandara_kode: "JND",
+        bandara_nama: "Juanda",
+    },
+    ADS:{
+        bandara_kode: "ADS",
+        bandara_nama: "Adi Sucipto",
+    },
+    KLN:{
+        bandara_kode: "KLN",
+        bandara_nama: "Kualanamu",
+    },
+};
+
+export const JADWAL = [
+    {
+        jadwal_id: "1",
+        bandara_kode_keberangkatan: "SKH",
+        bandara_kode_tujuan: "RDN",
+        maskapai_id: "GRD"
+    },
+    {
+        jadwal_id: "2",
+        bandara_kode_keberangkatan: "SKH",
+        bandara_kode_tujuan: "NGR",
+        maskapai_id: "LIO"
+    },
+    {
+        jadwal_id: "3",
+        bandara_kode_keberangkatan: "RDN",
+        bandara_kode_tujuan: "NGR",
+        maskapai_id: "SJW"
+    },
+    {
+        jadwal_id: "4",
+        bandara_kode_keberangkatan: "NGR",
+        bandara_kode_tujuan: "SKH",
+        maskapai_id: "GRD"
+    },
+    {
+        jadwal_id: "5",
+        bandara_kode_keberangkatan: "JND",
+        bandara_kode_tujuan: "ADS",
+        maskapai_id: "CTL"
+    },
+    {
+        jadwal_id: "6",
+        bandara_kode_keberangkatan: "JND",
+        bandara_kode_tujuan: "KLN",
+        maskapai_id: "AJT"
+    },
+    {
+        jadwal_id: "7",
+        bandara_kode_keberangkatan: "ADS",
+        bandara_kode_tujuan: "KLN",
+        maskapai_id: "BTK"
+    },
+    {
+        jadwal_id: "8",
+        bandara_kode_keberangkatan: "NGR",
+        bandara_kode_tujuan: "ADS",
+        maskapai_id: "GRD"
+    },
+    {
+        jadwal_id: "9",
+        bandara_kode_keberangkatan: "SKH",
+        bandara_kode_tujuan: "KLN",
+        maskapai_id: "LIO"
+    },
+    {
+        jadwal_id: "10",
+        bandara_kode_keberangkatan: "RDN",
+        bandara_kode_tujuan: "ADS",
+        maskapai_id: "SJW"
+    },
+    {
+        jadwal_id: "11",
+        bandara_kode_keberangkatan: "SKH",
+        bandara_kode_tujuan: "ADS",
+        maskapai_id: "BTK"
+    },
+    {
+        jadwal_id: "12",
+        bandara_kode_keberangkatan: "JND",
+        bandara_kode_tujuan: "RDN",
+        maskapai_id: "BTK"
+    },
+    {
+        jadwal_id: "13",
+        bandara_kode_keberangkatan: "SKH",
+        bandara_kode_tujuan: "RDN",
+        maskapai_id: "CTL"
+    },
+    {
+        jadwal_id: "14",
+        bandara_kode_keberangkatan: "ADS",
+        bandara_kode_tujuan: "SKH",
+        maskapai_id: "AJT"
+    },
+    {
+        jadwal_id: "15",
+        bandara_kode_keberangkatan: "KLN",
+        bandara_kode_tujuan: "SKH",
+        maskapai_id: "GRD"
+    },
+    {
+        jadwal_id: "16",
+        bandara_kode_keberangkatan: "KLN",
+        bandara_kode_tujuan: "SKH",
+        maskapai_id: "GRD"
+    },
+    {
+        jadwal_id: "17",
+        bandara_kode_keberangkatan: "KLN",
+        bandara_kode_tujuan: "SKH",
+        maskapai_id: "CTL"
+    },
+    {
+        jadwal_id: "18",
+        bandara_kode_keberangkatan: "KLN",
+        bandara_kode_tujuan: "SKH",
+        maskapai_id: "WNG"
+    },
+    {
+        jadwal_id: "19",
+        bandara_kode_keberangkatan: "KLN",
+        bandara_kode_tujuan: "SKH",
+        maskapai_id: "AJT"
+    },
+    {
+        jadwal_id: "20",
+        bandara_kode_keberangkatan: "KLN",
+        bandara_kode_tujuan: "SKH",
+        maskapai_id: "SJW"
+    },
+
+];
